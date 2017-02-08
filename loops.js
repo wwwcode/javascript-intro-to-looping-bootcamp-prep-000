de04fcc80d8c5fafe55fec3d796f70d783170258
Tayeb Mc <gomail654@gmail.com>
2017-02-08 15:21:43 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,25 @@
+var makeArray = [];
+function forLoop(){
+  for ( i = 0; i < 25; i++) {
+     makeArray.push("I am " + i +" strange loop" + (i === 0 ? "":"s")+ ".");
+}
+         return [makeArray]
+}
+var n = 0;
+function whileLoop(n){
+
+     while (n > 0) {
+    console.log("done", n);
+      n--;
+  }
+}
+function doWhileLoop(array){
+  var makeArray = [];
+  makeArray.length = l;
+      do{
+           true
+         }
+        while (makeArray.lenth < array );
+        return false
+
+}
