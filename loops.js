719c4bc5f1cf09e52c5e07e6a00b5718383c4777
Tayeb Mc <gomail654@gmail.com>
2018-01-10 17:17:44 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -21,14 +21,14 @@ function whileLoop(n){
 }
 
 
-function doWhileLoop(array){
-     maybeTrue() {
-        return Math.random() >= 0.5;
-     }
-      do{
-           array = array.slice(1);
-         }
-        while (array.length > 0 && maybeTrue());
-        return array;
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
 
+  do {
+    array = array.slice(1)
+  } while (array.length > 0 && maybeTrue())
+
+  return array
 }
