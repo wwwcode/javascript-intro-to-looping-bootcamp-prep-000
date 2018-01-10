53a4ae581aa78c14d6f639181a35325d1dd15305
Tayeb Mc <gomail654@gmail.com>
2018-01-10 17:03:41 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,12 @@
-
+var makeArray = [];
 function forLoop(array){
   for (let i = 0; i < 25; i++) {
-     array.push("I am " + i +" strange loop" + (i === 0 ? "":"s")+ ".");
-}
-         return [array]
+       if (i === 1){
+          array.push("I am 1 strange loop");
+       }else{
+          array.push("I am ${i} strange loops");
+    }
+
 }
 
 
